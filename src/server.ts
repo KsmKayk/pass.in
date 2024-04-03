cc9243f9e3c6076f7da80bd95bade526bf852430
Kayk Mascarenhas <kaykdsg@gmail.com>
2024-04-03 16:58:37 -0300

    chore: separate prisma instance and route to files

--- src/server.ts
+++ src/server.ts
@@ -1,50 +1,14 @@
 import fastify from "fastify";
-import z from "zod";
-import { PrismaClient } from "@prisma/client";
-import { generateSlug } from "./utils/generate-slug";
+import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import { createEvent } from "./routes/create-event";
 
 const app = fastify();
 
-const prisma = new PrismaClient({
-  log: ["query"],
-});
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
-app.get("/", async (request, reply) => {
-  return { message: "Hello World" };
-});
+app.register(createEvent)
 
-app.post("/events", async (request, reply) => {
-  const createEventSchema = z.object({
-    title: z.string().min(4),
-    details: z.string().nullable(),
-    maximumAttendees: z.number().int().positive().nullable(),
-  })
-
-  const { title, details, maximumAttendees } = createEventSchema.parse(request.body);
-
-  const slug = generateSlug(title);
-
-  const eventWithSameSlug = await prisma.event.findUnique({
-    where: { slug }
-  })
-
-  if (eventWithSameSlug !== null) {
-    throw new Error("An event with the same title already exists");
-  }
-
-
-
-  const event = await prisma.event.create({
-    data: {
-      title,
-      details,
-      maximumAttendees,
-      slug
-    }
-  })
-
-  return { eventId: event.id };
-});
 
 app.listen({ port: 3333 }).then(() => {
   console.log("Server is running on port 3333");
